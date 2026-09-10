8747b95f29566f3b0ea1c89f65c692e4437a2d2d
viresh anand <vireshanand278@gmail.com>
2026-09-10 23:14:49 +0530

    first redirection

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/mongo.config.js";
+import schema from "./src/models/shorturl.model.js";
 
 dotenv.config();
-connectDB();
+
 
 const app=express();
 import {nanoid} from "nanoid";
@@ -13,11 +14,29 @@ app.use(express.urlencoded({extended:true}));
 
 app.post("/api/create",(req,res)=>{
     const {url}=req.body;
+    const shortUrl=nanoid(7);
+    const newUrl=new schema({
+        full_url:url,
+        short_url:shortUrl
+    });
+    newUrl.save();
     console.log(url);
     res.send(nanoid(7));
 })
 
+app.get("/:shortUrl",async(req,res)=>{
+    const id=req.params.shortUrl;
+    const url=await schema.findOne({short_url:id});
+   
+    if(url){
+        res.redirect(url.full_url);
+    }else{
+        res.status(404).send("URL not found");
+    }
+});
+
 app.listen(5000,()=>{
+    connectDB();
     console.log("Server is runing on http://localhost:5000");
 })
 
